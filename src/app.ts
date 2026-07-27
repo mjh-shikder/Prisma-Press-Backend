7c843f54ee8685567976cd7bd536b53f4f35545c
mjh-shikder <mjhsikder@gmail.com>
2026-07-27 19:50:49 +0600

    profile model added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors"
 import config from "./config";
+import { prisma } from "./lib/prisma";
+import { log } from "console";
 
 const app: Application = express();
 
@@ -17,7 +19,7 @@ app.use(cookieParser())
 
 //?
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {    
     res.send("Hellow, workd")
 });
 
