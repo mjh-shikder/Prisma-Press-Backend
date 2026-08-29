bfece50b1f02645a523b9d79299c3be462d36d93
mjh-shikder <mjhsikder@gmail.com>
2026-08-29 06:32:34 +0600

    comment and post modules APIs and function added

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,8 @@ import  httpStatus  from "http-status";
 import bcrypt from "bcryptjs";
 import { userRouter } from "./modules/users/user.route";
 import { authRoutes } from "./modules/auth/auth.routes";
+import { postRoutes } from "./modules/post/post.route";
+import { commentRoutes } from "./modules/comment/comment.route";
 
 const app: Application = express();
 
@@ -25,6 +27,8 @@ app.use(cookieParser())
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRoutes)
+app.use("api/posts", postRoutes)
+app.use("/api/comments", commentRoutes)
 
 app.get("/", async (req: Request, res: Response) => {    
     res.send("Hellow, workd")
---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,40 @@
+
+
+const createPost = () => {
+    
+}
+
+const getAllPosts = () => {
+
+}
+
+const getPostsById = () => {
+
+}
+
+const updatePost = () => {
+
+}
+
+const deletePost = () => { 
+
+}
+
+const getPostsStatus = () => {
+
+}
+
+const getMyPosts = () => {
+
+}
+
+
+export const postService = {
+    createPost,
+    getAllPosts,
+    getPostsById,
+    updatePost,
+    deletePost,
+    getPostsStatus,
+    getMyPosts
+}
