ad9c0b268d7658b20a00261e0b5a13835d7c0f26
mjh-shikder <mjhsikder@gmail.com>
2026-09-08 06:08:03 +0600

    Subscription model added

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { Prisma } from "../../generated/prisma/client";
 
-
 export const globalErrorHandler = (
   err: any,
   req: Request,
@@ -11,38 +10,38 @@ export const globalErrorHandler = (
 ) => {
   console.log(err);
 
-  let statusCode
+  let statusCode;
   let errorMessage = err.message || "Internal Server Error";
   let errorName = err.name || "Internal Server Error";
 
-
   if (err instanceof Prisma.PrismaClientValidationError) {
-     statusCode = httpStatus.BAD_REQUEST;
-     errorMessage = "You have provided Incorrect field type or missing fields"
-} else if (err instanceof Prisma.PrismaClientKnownRequestError) {
-   if(err.code === "P2002") {
-    statusCode = httpStatus.CONFLICT;
-    errorMessage = "Duplicate field value entered";
-   } else if(err.code === "P2003") {
     statusCode = httpStatus.BAD_REQUEST;
-    errorMessage = "Foreign key constraint failed";
-   } else if (err.code === "P2025") {
-    statusCode = httpStatus.BAD_REQUEST;
-    errorMessage = "An operation failed because it depends on one or more records that were required but not found.";
-   }
-  } else if ( err instanceof Prisma.PrismaClientInitializationError) {
+    errorMessage = "You have provided Incorrect field type or missing fields";
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = httpStatus.CONFLICT;
+      errorMessage = "Duplicate field value entered";
+    } else if (err.code === "P2003") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage = "Foreign key constraint failed";
+    } else if (err.code === "P2025") {
+      statusCode = httpStatus.BAD_REQUEST;
+      errorMessage =
+        "An operation failed because it depends on one or more records that were required but not found.";
+    }
+  } else if (err instanceof Prisma.PrismaClientInitializationError) {
     if (err.errorCode === "P1000") {
       statusCode = httpStatus.UNAUTHORIZED;
-      errorMessage = "Authentication failed agains database server. Please check your database credentials and try again.";
-   }else if (err.errorCode === "P1001") {
+      errorMessage =
+        "Authentication failed agains database server. Please check your database credentials and try again.";
+    } else if (err.errorCode === "P1001") {
       statusCode = httpStatus.BAD_REQUEST;
       errorMessage = "Can't Reach database server";
-   }
+    }
   } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
     statusCode = httpStatus.INTERNAL_SERVER_ERROR;
     errorMessage = "An unknown error occurred while processing the request.";
   }
-
 
   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
     success: false,
