2e378e41012a1104b77b4d41624529940f2edb54
mjh-shikder <mjhsikder@gmail.com>
2026-09-09 07:19:09 +0600

    setting up stripe webhook endpoint route for subscription

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ import { notFound } from "./middlewares/notFound";
 import httpStatus from "http-status";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { subscriptionRoutes } from "./modules/subscription/subscription.route";
+import { stripe } from "./lib/stripe";
 
 const app: Application = express();
 
@@ -20,6 +21,67 @@ app.use(
     credentials: true,
   }),
 );
+
+const endpointSecret = config.stripe_webhook_secret
+
+app.post(
+  "/api/subscription/webhook",express.raw({ type: "application/json" }),
+  (request, response) => {
+    let event = request.body;
+    console.log(event, "Stripe Request Body");
+    console.log(request.header, "stripe req headers");
+    
+    
+      // Only verify the event if you have an endpoint secret defined.
+      // Otherwise use the basic event deserialized with JSON.parse
+      if (endpointSecret) {
+        // Get the signature sent by Stripe
+        const signature = request.headers["stripe-signature"]!;
+        try {
+          event = stripe.webhooks.constructEvent(
+            request.body,
+            signature,
+            endpointSecret,
+          );
+        } catch (err: any) {
+          console.log(
+            `⚠️  Webhook signature verification failed.`,
+            err.message,
+          );
+          return response.status(400).json({
+            mesage: err.message
+          });
+        }
+    }
+    
+    console.log(event, "event after try block");
+    
+
+      // Handle the event
+      switch (event.type) {
+        case "payment_intent.succeeded":
+          const paymentIntent = event.data.object;
+          console.log(
+            `PaymentIntent for ${paymentIntent.amount} was successful!`,
+          );
+          // Then define and call a method to handle the successful payment intent.
+          // handlePaymentIntentSucceeded(paymentIntent);
+          break;
+        case "payment_method.attached":
+          const paymentMethod = event.data.object;
+          // Then define and call a method to handle the successful attachment of a PaymentMethod.
+          // handlePaymentMethodAttached(paymentMethod);
+          break;
+        default:
+          // Unexpected event type
+          console.log(`Unhandled event type ${event.type}.`);
+      }
+
+      // Return a 200 response to acknowledge receipt of the event
+      response.send();
+  }
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -30,14 +92,13 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/api/subscription", subscriptionRoutes)
+app.use("/api/subscription", subscriptionRoutes);
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hellow, workd");
 });
 
-
-app.use(notFound)
+app.use(notFound);
 
 app.use(globalErrorHandler);
 
